3b5baac1eacc22429c55c31b3b7f7d2331fc354e
Rico Sonntag <mail@ricosonntag.de>
2026-05-27 22:40:22 +0200

    Bump version to 1.1.1-dev

--- src/Module.php
+++ src/Module.php
@@ -50,7 +50,7 @@ final class Module extends StatisticsChartModule implements ModuleAssetUrlInterf
 
     public const string CUSTOM_AUTHOR = 'Rico Sonntag';
 
-    public const string CUSTOM_VERSION = '1.1.0';
+    public const string CUSTOM_VERSION = '1.1.1-dev';
 
     /**
      * Webtrees renders this URL as the "For more information, see …" link
