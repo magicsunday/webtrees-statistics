4eae45166a2db1e3d8a9234d3fe20dbca056c4e7
Rico Sonntag <mail@ricosonntag.de>
2026-06-21 17:26:29 +0200

    Release 1.8.0

--- src/Module.php
+++ src/Module.php
@@ -50,7 +50,7 @@ final class Module extends StatisticsChartModule implements ModuleAssetUrlInterf
 
     public const string CUSTOM_AUTHOR = 'Rico Sonntag';
 
-    public const string CUSTOM_VERSION = '1.7.2-dev';
+    public const string CUSTOM_VERSION = '1.8.0';
 
     /**
      * Webtrees renders this URL as the "For more information, see …" link
